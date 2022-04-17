71ee366ecbcd59fbbe0206f7bb28bc30fd1d738c
priyankapeelu <priyankapeelu@icloud.com>
2022-04-17 16:12:52 +0530

    terraform variables

--- example1/ec2.tf
+++ example1/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "sample" {
   ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = [sg-03932f0b41e54e564]
 }
 output "public_ip" {
   value = aws_instance.sample.public_ip
