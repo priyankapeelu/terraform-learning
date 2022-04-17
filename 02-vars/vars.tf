82a7ebb5f6a2be1ccc6cff0451bec38d9c989757
priyankapeelu <priyankapeelu@icloud.com>
2022-04-17 15:36:08 +0530

    terraform variables

---
+++ 02-vars/vars.tf
@@ -0,0 +1,45 @@
+variable "example" {
+  default = "Hello World"
+}
+
+output "example" {
+  value = var.example
+}
+
+## Data types
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+
+### Variable Types
+variable "sample4" {
+  type = list(string) ## optional
+  default = [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sample5" {
+  type = map(any) ## optional
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
