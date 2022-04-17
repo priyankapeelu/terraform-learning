384a7b4f67ec2ce100110055b54c4f98c7056881
priyankapeelu <priyankapeelu@icloud.com>
2022-04-17 16:21:13 +0530

    terraform variables

--- example1/ec2.tf
+++ example1/ec2.tf
@@ -10,3 +10,29 @@ output "public_ip" {
   value = aws_instance.sample.public_ip
 }
 
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
