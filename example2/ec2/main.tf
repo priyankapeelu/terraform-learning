a23dbd55b6aaa59bd7173aea11a6a964dcbc08f7
priyankapeelu <priyankapeelu@icloud.com>
2022-04-18 15:54:31 +0530

    terraform variables

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "sample" {
   ami                    = data.aws_ami.example.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg]
+  provisioner "local-exec" {
+    command = "sleep 60; "
+  }
 }
 
 variable "sg" {}
