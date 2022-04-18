33aeaeabfb9ec4a6f0d14ecfcc9f63aeed8ef52e
priyankapeelu <priyankapeelu@icloud.com>
2022-04-18 15:33:44 +0530

    terraform variables

--- example2/ec2/data.tf
+++ example2/ec2/data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "example" {
   executable_users = ["self"]
   most_recent = true
-  name_regex  = "base_with_ansible"
+  name_regex  = "base_ami_with_ansible"
   owners      = ["self"]
 }
