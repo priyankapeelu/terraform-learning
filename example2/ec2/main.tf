20b34d5d4d6101664d1753fea126e1943b019790
priyankapeelu <priyankapeelu@icloud.com>
2022-04-18 15:30:46 +0530

    terraform variables

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami                    = data.aws_ami.image_id
+  ami                    = data.aws_ami.example.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg]
 }
