a5a154ea8d7c8d66e9ce963eeb38828734b578d7
priyankapeelu <priyankapeelu@icloud.com>
2022-04-19 16:00:55 +0530

    terraform variables

--- roboshop-basic-setup/ec2/sg.tf
+++ roboshop-basic-setup/ec2/sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_all" {
-  name        = "allow_${var.COMPONENT}}"
+  name        = "allow_${var.COMPONENT}"
   description = "Allow TLS inbound traffic"
-
+}
   ingress {
     description = "TLS from VPC"
     from_port   = 0
@@ -15,11 +15,10 @@ resource "aws_security_group" "allow_all" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "allow_${var.COMPONENT}}"
+    Name = "allow_${var.COMPONENT}"
   }
-}
+
 
