ff3b49ce2f8a9d31093de4bc4ff16be84c8e3849
priyankapeelu <priyankapeelu@icloud.com>
2022-04-19 16:44:57 +0530

    terraform variables

--- roboshop-basic-setup/ec2/ec2.tf
+++ roboshop-basic-setup/ec2/ec2.tf
@@ -15,7 +15,6 @@ resource "aws_spot_instance_request" "cheap_worker" {
       password = "DevOps321"
     }
     inline = [
-      "ansible|sudo labauto ",
       "ansible-pull -U https://github.com/priyankapeelu/ansible roboshop.yml -e HOST=localhost -e role_name=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION}"
     ]
   }
