960729cc4c8b5ad88e3b71afc20e5e68541a033e
priyankapeelu <priyankapeelu@icloud.com>
2022-04-20 16:53:34 +0530

    terraform variables

--- roboshop-basic-setup/main.tf
+++ roboshop-basic-setup/main.tf
@@ -1,26 +1,6 @@
-module "COMPONENT" {
-  count       =  var.ALL_COMPONENTS
-  source      =  "./ec2"
-  COMPONENT   =  each.key
-  APP_VERSION =  each.value
+module "component" {
+  for_each    = var.ALL_COMPONENTS
+  source      = "./ec2"
+  COMPONENT   = each.key
+  APP_VERSION = each.value.app_version
 }
-
-
-#resource "null_resource" "sample" {
-#  triggers = {
-#    abc = timestamp()
-#  }
-#  for_each = var.sample
-#  provisioner "local-exec" {
-#    command = <<EOF
-#echo COMPONENT = ${each.key}, APP_VERSION = ${each.value}
-#EOF
-#  }
-#}
-
-
-
-
-
-
-
